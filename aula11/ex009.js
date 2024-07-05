c6aad4faf71b892ff1b41cf5a958944cdbbf83f4
Gustavo Pimentel <gugahpimentel@hotmail.com>
2024-07-05 11:10:22 -0300

    Condições em JS

---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var time = 'Flamengo'
+if(time == 'Flamengo'){
+    console.log('Você torce pro Flamengo, parabéns você é inteligente!!')
+} else {
+    console.log('Você não torce pro Flamengo, você é burro??')
+}

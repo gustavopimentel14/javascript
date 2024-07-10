c4415b04a96fdfec4dee3bcac7ed6bf9aeb6a6f1
Gustavo Pimentel <gugahpimentel@hotmail.com>
2024-07-10 10:18:23 -0300

    Praticando JS

---
+++ exercicios/ex-extras/exextra03/script1.js
@@ -0,0 +1,13 @@
+var res = document.getElementById('resultado')
+function clicarum(){
+    res.innerHTML = `<p>Agora você torce para o Flamengo</p>`
+}
+function clicardois(){
+    res.innerHTML = `<p>Agora você me deve R$100</p>`
+}
+function clicartres(){
+    res.innerHTML = `<p>Parabéns você apertou no terceiro botão. Não ganhou nada.</p>`
+}
+function clicarquatro(){
+    res.innerHTML = `<p>Oi</p>`
+}

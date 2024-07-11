c0951722cd4c4f8d9191396bbb609ee991f47a74
Gustavo Pimentel <gugahpimentel@hotmail.com>
2024-07-11 11:35:14 -0300

    Mais ex em JS

--- exercicios/ex02/interaçao/script.js
+++ exercicios/ex02/interaçao/script.js
@@ -1,6 +1,34 @@
 function verificar(){
+    var data = new Date()
+    var nano = data.getFullYear()
     var nasc = document.getElementById('iano')
     var res = document.getElementById('res')
-    var ano = Number(nasc.value)
-    res.innerHTML = `Você nasceu no ano de ${ano}.`
+    if(Number(nasc.value) < 1920 || Number(nasc.value) > nano){
+        window.alert('Não foi possível calcular. Verifique os dados e tente novamente.')
+    } else{
+        var sex = document.getElementsByName('sexo')
+        var idade = nano - Number(nasc.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (sex[0].checked){
+        genero = 'masculino'
+            if (idade >= 0 && idade < 12){
+                //criança
+                img.setAttribute('src', '../imagens/criança-m.png')
+            } else if(idade < 21){
+                //jovem
+                img.setAttribute('src', '../imagens/adolescente-m.png')
+            } else if(idade < 60){
+                //adulto
+                img.setAttribute('src', '../imagens/adulto-m.jpg')
+            } else if (idade > 60){
+                //idoso
+                img.setAttribute('src', '..imagens/velhinho.png')
+            }
+        } else if(sex[1].checked){
+            genero = 'feminino'
+        }
+        res.innerHTML = `${genero} de ${idade} anos.`
+    }
 }

207d58b1a2ba89925dd946f2347904e47d0b4e56
Gustavo Pimentel <gugahpimentel@hotmail.com>
2024-07-12 10:22:34 -0300

    repetições em JS

---
+++ aula13/ambiente.js
@@ -0,0 +1,25 @@
+var a = 1
+do {
+    console.log(`Flamengo ${a}`)
+    a++
+} while(a<=10)
+/*
+var a = 1
+while(a<=10){
+    console.log(`Flamengo ${a}`)
+    a++
+}
+*/
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/

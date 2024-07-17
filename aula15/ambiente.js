5b6bc1ce4b8ec5605bcb88e76d36f0cc89440a53
Gustavo Pimentel <gugahpimentel@hotmail.com>
2024-07-17 10:43:26 -0300

    Funções

---
+++ aula15/ambiente.js
@@ -0,0 +1,7 @@
+let num = [4, 3, 6, 5, 9]
+num[5] = 1
+num.push(7)
+num.sort()
+console.log(`Meu vetor é o ${num}`)
+console.log(`${num}`)
+console.log(`Meu vetor tem ${num.length} posições.`)
---
+++ aula15/vetornatela.js
@@ -0,0 +1,9 @@
+let gol = [1, 2, 4, 7, 0]
+/*
+for(let pos=0; pos<gol.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${gol[pos]}`)
+}
+*/
+for(let pos in gol){
+    console.log(`A posição ${pos} tem o valor ${gol[pos]}`)
+}
